719cb61e40880c7dc5b690358ae9bf2c9456f246
farad213 <farad213@gmail.com>
2022-12-01 18:46:26 +0100

    CSRF_COOKIE_SECURE = True

--- nyuzzs/settings.py
+++ nyuzzs/settings.py
@@ -127,3 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
